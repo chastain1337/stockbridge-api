8eb8f3b960ba29e58191c98b40b6af8ce5cdd5e4
James Chastain <jchastain@strategicmarketing.com>
2020-08-24 11:08:00 -0400

    8/24

--- StockBridge/Repositories/BaseRepository.cs
+++ StockBridge/Repositories/BaseRepository.cs
@@ -9,8 +9,9 @@ namespace StockBridge.Repositories
         public readonly IDbConnection _db;
         protected BaseRepository()
         {
-            _dbConnectionString = "Server=localhost;Database=StockBridge;User Id=sa;Password=White434$;";
-            //_dbConnectionString = "Server=\"(localdb)\\MSSQLLocalDB\";Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            //_dbConnectionString = "Server=localhost;Database=StockBridge;User Id=sa;Password=White434$;";
+            _dbConnectionString =
+                "Server=\"(localdb)\\MSSQLLocalDB\";Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             _db = new SqlConnection(_dbConnectionString);
         }
     }
